87a2625934dabb6c93fec2fef5a8d98909295129
Jacob Gardner <jacobjamesg1999@gmail.com>
2025-01-10 15:32:43 +0000

    Add add list functionality

--- store/store.go
+++ store/store.go
@@ -30,10 +30,27 @@ func NewInMemoryStore() *InMemoryStore {
 	}
 }
 
+func NewUser(id, name string) User {
+	return User{
+		ID:        id,
+		Name:      name,
+		TodoLists: make(map[string]TodoList),
+	}
+}
+
 func (s *InMemoryStore) CreateUser(user User) error {
 	if _, exists := s.users[user.ID]; exists {
 		return fmt.Errorf("user with ID %s already exists", user.ID)
 	}
 	s.users[user.ID] = user
+	return nil
+}
+
+func (u *User) AddTodoList(list TodoList) error {
+	if _, exists := u.TodoLists[list.ID]; exists {
+		return fmt.Errorf("list with ID %s for user %s already exists", list.ID, u.ID)
+	}
+
+	u.TodoLists[list.ID] = list
 	return nil
 }
--- store/store_test.go
+++ store/store_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestAddUser(t *testing.T) {
 	store := NewInMemoryStore()
 
-	user := User{ID: "0001", Name: "Steve"}
+	user := NewUser("0001", "Steve")
 	store.CreateUser(user)
 
 	got := store.users["0001"].Name
@@ -19,8 +19,8 @@ func TestAddUser(t *testing.T) {
 func TestAddDuplicateUserID(t *testing.T) {
 	store := NewInMemoryStore()
 
-	user := User{ID: "001", Name: "Steve"}
-	user2 := User{ID: "001", Name: "Stephen"}
+	user := NewUser("0001", "Steve")
+	user2 := NewUser("0001", "Stephen")
 
 	store.CreateUser(user)
 	err := store.CreateUser(user2)
@@ -29,7 +29,47 @@ func TestAddDuplicateUserID(t *testing.T) {
 		t.Fatal("expected an error")
 	}
 
-	want := "user with ID 001 already exists"
+	want := "user with ID 0001 already exists"
+
+	if err.Error() != want {
+		t.Errorf("got %q want %q", err.Error(), want)
+	}
+}
+
+func TestAddList(t *testing.T) {
+	store := NewInMemoryStore()
+
+	user := NewUser("0001", "Steve")
+	store.CreateUser(user)
+
+	list := TodoList{ID: "0001", Name: "test list"}
+	user.AddTodoList(list)
+
+	got := len(user.TodoLists)
+	want := 1
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestAddDuplicateListID(t *testing.T) {
+	store := NewInMemoryStore()
+
+	user := NewUser("0001", "Steve")
+	store.CreateUser(user)
+
+	list := TodoList{ID: "0001", Name: "test list"}
+	list2 := TodoList{ID: "0001", Name: "duplicate list"}
+	user.AddTodoList(list)
+
+	err := user.AddTodoList(list2)
+
+	if err == nil {
+		t.Fatal("expected an error")
+	}
+
+	want := "list with ID 0001 for user 0001 already exists"
 
 	if err.Error() != want {
 		t.Errorf("got %q want %q", err.Error(), want)
